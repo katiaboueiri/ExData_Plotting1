d8adc39b0627deef64db8cf7010aa96e6dcc2b60
katiaboueiri <40272817+katiaboueiri@users.noreply.github.com>
2018-10-18 10:39:00 +0300

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,26 @@
+# Coursera Data Science: Exploratory Data Analysis
+# Course project 1: plot2
+################################################################################
+## load the data
+rm(list = ls())
+data <- read.table("household_power_consumption.txt", header = T, 
+                   sep = ";", na.strings = "?")
+# convert the date variable to Date class
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+
+# Subset the data
+data <- subset(data, subset = (Date >= "2007-02-01" & Date <= "2007-02-02"))
+
+# Convert dates and times
+data$datetime <- strptime(paste(data$Date, data$Time), "%Y-%m-%d %H:%M:%S")
+data$datetime <- as.POSIXct(data$datetime)
+
+# Plot 2
+png("plot2.png", width=480, height=480)
+
+attach(data)
+plot(Global_active_power ~ datetime, type = "l",
+     ylab = "Global Active Power (kilowatts)", xlab = "")
+
+dev.off()
+detach(data)
---
+++ plot3.R
@@ -0,0 +1,30 @@
+# Coursera Data Science: Exploratory Data Analysis
+# Course project 1: plot3
+################################################################################
+## load the data
+rm(list = ls())
+data <- read.table("household_power_consumption.txt", header = T, 
+                   sep = ";", na.strings = "?")
+# convert the date variable to Date class
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+
+# Subset the data
+data <- subset(data, subset = (Date >= "2007-02-01" & Date <= "2007-02-02"))
+
+# Convert dates and times
+data$datetime <- strptime(paste(data$Date, data$Time), "%Y-%m-%d %H:%M:%S")
+data$datetime <- as.POSIXct(data$datetime)
+
+# Plot 3
+png("plot3.png", width=480, height=480)
+
+attach(data)
+plot(Sub_metering_1 ~ datetime, type = "l", 
+     ylab = "Energy sub metering", xlab = "")
+lines(Sub_metering_2 ~ datetime, col = "Red")
+lines(Sub_metering_3 ~ datetime, col = "Blue")
+legend("topright", lty = 1, col = c("black", "red", "blue"), 
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+dev.off()
+detach(data)
